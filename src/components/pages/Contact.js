88666310c9bd17c16232f1b565da3603961f14dc
Steve Koutsodontis <skoutsodontis@gmail.com>
2022-01-15 19:42:10 -0500

    added contact information with links and img's

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,31 +1,36 @@
 import React from 'react';
+import Email from '../images/gmail_logo.png'
+import LinkedIn from '../images/LinkedIn_logo_.png'
+import GitHub from '../images/github-logo1.jpg'
+import Resume from '../images/resumeimg.PNG'
 
 export default function Contact() {
   return (
-    <div id="Contact-Info" class="Links">
+    <div class="contact">
       <h1>Contact Page</h1>
       <p>
-        Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-        molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-        magna a ultrices. Aenean pellentesque placerat lacus imperdiet
-        efficitur. In felis nisl, luctus non ante euismod, tincidunt bibendum
-        mi. In a molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna
-        posuere, eget tristique dui dapibus. Maecenas fermentum elementum
-        faucibus. Quisque nec metus vestibulum, egestas massa eu, sollicitudin
-        ipsum. Nulla facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh
-        dictum ullamcorper. Class aptent taciti sociosqu ad litora torquent per
-        conubia nostra, per inceptos himenaeos. Etiam ornare rutrum felis at
-        rhoncus. Etiam vel condimentum magna, quis tempor nulla.
+        Please feel free to contact me at anytime. Through these links you will be able to Email, Check my Github, My LinkedIn and also download a copy of my resume.
       </p>
-        <h2>Contact Info</h2>
-        <p>Phone: 6319426067</p>
+        <footer>Contact Info</footer>
         <p>Email: Skoutsodontis@gmail.com</p>
-        <a href="mailto:skoutsodontis@gmail.com">
-        <img src="./src/assets/images/gmail_logo.png" alt="myemails" /></a>
-        <a href="https://www.linkedin.com/in/steve-koutsodontis-937264127"><img src="./assets/images/LinkedIn_logo_.png" alt="LinkedIn link" /></a>
-        <a href="https://github.com/SteveKoutsodontis"><img src="./assets/images/github-logo1.jpg" alt="https://github.com/SteveKoutsodontis" /></a>
-        <a href="./assets/images/SK_Res_1.pdf" download="SK_Res_1">
-          <img src="./assets/images/resumeimg.PNG" alt="skres" width="104" height="142"/></a>
+        <a className='contactEmail'
+        href="mailto:skoutsodontis@gmail.com">
+          <img src={Email}/>
+          </a>
+        
+        <a className='contactLinkedIn'
+         href="https://www.linkedin.com/in/steve-koutsodontis-937264127">
+           <img src={LinkedIn}></img>
+           </a>
+
+        <a className='contactGitHub'
+        href="https://github.com/SteveKoutsodontis">        
+        <img src={GitHub}></img>
+        </a>
+        <a className='contactResume' href="./assets/images/SK_Res_1.pdf" download="SK_Res_1.pdf">
+        <img src={Resume} width="204" height="242"></img>
+        </a>
+          
     </div>
   )
   }
